4b3cd5240847af5c5abc784e4f233f965a9b41e0
Torsten Anders <torsten.anders@beds.ac.uk>
2017-07-12 19:01:11 +0200

    Updated loading of files with __package.lisp

--- __package.lisp
+++ __package.lisp
@@ -1,53 +1,69 @@
 ;;;;; BEFORE : RUN SBCL WITH THE CORE OF MOZ LIB
 
-(defpackage cluster-engine (:use :common-lisp :sb-ext))
+(defpackage cluster-engine
+  #+SBCL
+  (:use :common-lisp :sb-ext)
+  #-SBCL
+  (:use :common-lisp)
+  (:nicknames :ce))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defparameter *current-directory* 
+  (pathname-directory *load-truename*))
+
+(defun load-local (lisp-file-name)
+  "Loads a lisp file in the directory of this file."
+  (load 
+   (make-pathname :directory *current-directory*
+                  :name lisp-file-name
+                  :type "lisp")))
 
 ;;;  :pw  ;;;;;
-(load "from-studio-flat")
-(load "01.domain.lisp")
-(load "02.engine.lisp")
-(load "03.Fwd-rules.lisp")
-(load "04.Backtrack-rules.lisp")
-(load "05.rules-interface.lisp")
-(load "05a.rules-interface-1engine.lisp")
-(load "05b.rules-interface-2engines.lisp")
-(load "05c.rules-interface-2engines.lisp")
-(load "05d.rules-interface-2engines.lisp")
-(load "05e.rules-interface-2engines.lisp")
-(load "05f.rules-interface-3engines.lisp")
-(load "05g.rules-interface-any-n-engines.lisp")
-(load "05h.rules-higher-level.lisp")
-(load "05i.rules-stop-search.lisp")
-(load "05n.rules-interface-nn-engines.lisp")
-(load "06.heuristic-rules-interface.lisp")
-(load "06a.heuristic-rules-interface-1engine.lisp")
-(load "06b.heuristic-rules-interface-2engines.lisp")
-(load "06c.heuristic-rules-interface-2engines.lisp")
-(load "06d.heuristic-rules-interface-2engines.lisp")
-(load "06e.heuristic-rules-interface-2engines.lisp")
-(load "06f.heuristic-rules-interface-3engines.lisp")
-(load "06g.heuristic-rules-interface-any-n-engines.lisp")
-(load "07.backjumping.lisp")
-(load "08.decode.lisp")
-(load "09.utilities.lisp")
-(load "09b.markov-tools.lisp")
-(load "09c.cluster-energy-profile.lisp")
-(load "12.debug-tools.lisp")
-(load "13.convert-pmc-rules.lisp")
+(load-local "from-studio-flat")
+(load-local "01.domain")
+(load-local "02.engine")
+(load-local "03.Fwd-rules")
+(load-local "04.Backtrack-rules")
+(load-local "05.rules-interface")
+(load-local "05a.rules-interface-1engine")
+(load-local "05b.rules-interface-2engines")
+(load-local "05c.rules-interface-2engines")
+(load-local "05d.rules-interface-2engines")
+(load-local "05e.rules-interface-2engines")
+(load-local "05f.rules-interface-3engines")
+(load-local "05g.rules-interface-any-n-engines")
+(load-local "05h.rules-higher-level")
+(load-local "05i.rules-stop-search")
+(load-local "05n.rules-interface-nn-engines")
+(load-local "06.heuristic-rules-interface")
+(load-local "06a.heuristic-rules-interface-1engine")
+(load-local "06b.heuristic-rules-interface-2engines")
+(load-local "06c.heuristic-rules-interface-2engines")
+(load-local "06d.heuristic-rules-interface-2engines")
+(load-local "06e.heuristic-rules-interface-2engines")
+(load-local "06f.heuristic-rules-interface-3engines")
+(load-local "06g.heuristic-rules-interface-any-n-engines")
+(load-local "07.backjumping")
+(load-local "08.decode")
+(load-local "09.utilities")
+(load-local "09b.markov-tools")
+(load-local "09c.cluster-energy-profile")
+(load-local "12.debug-tools")
+(load-local "13.convert-pmc-rules")
 
-(load "_000.main-interface.lisp")
+(load-local "_000.main-interface")
 
+#|
 ;;;;;; PW and Co.
 (load "~/Dropbox/cluster-to-Max-Orjan/other-libraries/iter.lisp")
 (load "~/Dropbox/cluster-to-Max-Orjan/other-libraries/pw-common-language.lisp")
 (load "~/Dropbox/cluster-to-Max-Orjan/other-libraries/pw_profile_functions.lisp")
+|#
 
 ;;;;;; gen-domains
-(load "_001.gen_domains.lisp")
+(load-local "_001.gen_domains")
 
 
 ;;;;;;;;;;; PLEASE un-comment the proper one and comment the other 
