71f6db6b498b16765023d00f40f55a10c46ed8a9
Orjansan <sandred@ad.umanitoba.ca>
2017-07-13 08:06:22 -0500

    Fixed

--- test_files/3c.lisp
+++ test_files/3c.lisp
@@ -1,7 +1,7 @@
 (in-package cluster-engine) 
-
+(print
 (cluster-engine::ClusterEngine 16 t nil 
 		'() 
 		'((4 4)) 
-		'(((1/4)) ((60 69)) (cluster-engine::m (-2 5 5)) (cluster-engine::m (-1 7)) (cluster-engine::m (1 9)) (cluster-engine::m (2 11)) )
-		)
+		'(((1/4)) (((60 69)) (m (-2 5 5)) (m (-1 7)) (m (1 9)) (m (2 11)) )  )
+		))
