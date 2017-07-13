242915b2215e494c3bac2c9f5b7819a8984f1099
JulienVincenot <julien.vincenot@gmail.com>
2017-07-13 15:05:22 +0200

    little changes test

--- test_files/3c.lisp
+++ test_files/3c.lisp
@@ -1,7 +1,7 @@
 (in-package cluster-engine) 
 
-(cluster-engine::ClusterEngine 16 t nil 
+(print (ClusterEngine 16 t nil 
 		'() 
 		'((4 4)) 
-		'(((1/4)) ((60 69)) (cluster-engine::m (-2 5 5)) (cluster-engine::m (-1 7)) (cluster-engine::m (1 9)) (cluster-engine::m (2 11)) )
-		)
+		'(((1/4)) ((60 69)) (cluster-engine::m (-2 5 5)) (cluster-engine::m (-1 7)) (m (1 9)) (cluster-engine::m (2 11)) )
+		))
