a454e72525b001ff4ec9a6150dd2e4d90061866e
Torsten Anders <torsten.anders@beds.ac.uk>
2017-07-13 15:24:20 +0200

    Minor package def cleanup

--- sources/package.lisp
+++ sources/package.lisp
@@ -1,7 +1,5 @@
 (defpackage cluster-engine
-  #+SBCL
-  (:use :common-lisp :sb-ext)
-  #-SBCL
-  (:use :common-lisp)
+  #+SBCL (:use :common-lisp :sb-ext)
+  #-SBCL (:use :common-lisp)
   (:nicknames :ce))
 
