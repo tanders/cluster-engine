27d22943d7a1b1a7bfb95ca726104c074bbcffd7
Torsten Anders <torsten.anders@beds.ac.uk>
2017-08-09 10:23:20 +0100

    Preview function name changed

--- test_files/TA-tests.lisp
+++ test_files/TA-tests.lisp
@@ -8,12 +8,12 @@
 |#
 
 
-(defun cluster-engine-score (score)
+(defun preview-cluster-engine-score (score)
   "Dummy def -- in my code this function notates and plays the score in Opusmodus."
   score)
 
 
-(cluster-engine-score
+(preview-cluster-engine-score
  (ce::ClusterEngine 
   10 t nil 
   ;; all rhythmic values are equal
@@ -25,7 +25,7 @@
 
 
 ;; Cluster Engine tutorial 5f 
-(cluster-engine-score
+(preview-cluster-engine-score
  (ce::ClusterEngine 
   20 t nil
   (ce::R-rhythms-one-voice-at-timepoints 
@@ -37,7 +37,7 @@
 
 
 ;; Cluster Engine tutorial 7c
-(cluster-engine-score
+(preview-cluster-engine-score
  (ce::ClusterEngine 
   10 t t 
   (append 
@@ -51,61 +51,60 @@
 
 
 ;; Cluster Engine tutorial 8a 
-(cluster-engine-score
-(ce::ClusterEngine 
- 12 t nil 
- (append 
-  (ce::R-pitches-one-voice 
-   #'(lambda (x) 
-       (not (member (mod (car (last x)) 12) (mapcar #'(lambda (a) (mod a 12)) (butlast x)) )))
-   '(0 1) :all-pitches)
-  (ce::R-pitch-pitch 
-   #'(lambda (x) 
-       (member (mod (ce::apply-minus x) 12) '(3 4 7 8 9))) '(0 1) '(0) :all :no_grace :pitch)
-  (ce::R-pitch-pitch #'(lambda (x) (>= (first x) (second x)) ) '(0 1) '(0) :all :no_grace :pitch)) 
- '((4 4)) 
- '(((1/4)) 
-   ((60) (61) (62) (63) (64) (65) (66) (67) (68) (69) (70) (71) (72) (73) (74) (75) (76) (77) (78) (79)) 
-   ((1/4)) 
-   ((60) (61) (62) (63) (64) (65) (66) (67) (68) (69) (70) (71) (72) (73) (74) (75) (76) (77) (78) (79))))
-)
+(preview-cluster-engine-score
+ (ce::ClusterEngine 
+  12 t nil 
+  (append 
+   (ce::R-pitches-one-voice 
+    #'(lambda (x) 
+        (not (member (mod (car (last x)) 12) (mapcar #'(lambda (a) (mod a 12)) (butlast x)) )))
+    '(0 1) :all-pitches)
+   (ce::R-pitch-pitch 
+    #'(lambda (x) 
+        (member (mod (ce::apply-minus x) 12) '(3 4 7 8 9))) '(0 1) '(0) :all :no_grace :pitch)
+   (ce::R-pitch-pitch #'(lambda (x) (>= (first x) (second x)) ) '(0 1) '(0) :all :no_grace :pitch)) 
+  '((4 4)) 
+  '(((1/4)) 
+    ((60) (61) (62) (63) (64) (65) (66) (67) (68) (69) (70) (71) (72) (73) (74) (75) (76) (77) (78) (79)) 
+    ((1/4)) 
+    ((60) (61) (62) (63) (64) (65) (66) (67) (68) (69) (70) (71) (72) (73) (74) (75) (76) (77) (78) (79))))
+ )
 
 
 ;; r-pitch-pitch 1st-beat
-(cluster-engine-score
-(ce::ClusterEngine 
- 12 t nil 	
- (ce::R-pitch-pitch 
-  #'(lambda (x) 
-      (member (mod (- (first x) (second x)) 12) '(3 4 7 8 9)))
-  '(0 1) '(0) :1st-beat :no_grace :pitch)
- '((4 4)) 
- '(((1/4)) 
-   ((60) (61) (62) (63) (64) (65) (66) (67) (68) (69) (70) (71) (72) (73) (74) (75) (76) (77) (78) (79)) 
-   ((1/4)) 
-   ((60) (61) (62) (63) (64) (65) (66) (67) (68) (69) (70) (71) (72) (73) (74) (75) (76) (77) (78) (79)))))
+(preview-cluster-engine-score
+ (ce::ClusterEngine 
+  12 t nil 	
+  (ce::R-pitch-pitch 
+   #'(lambda (x) 
+       (member (mod (- (first x) (second x)) 12) '(3 4 7 8 9)))
+   '(0 1) '(0) :1st-beat :no_grace :pitch)
+  '((4 4)) 
+  '(((1/4)) 
+    ((60) (61) (62) (63) (64) (65) (66) (67) (68) (69) (70) (71) (72) (73) (74) (75) (76) (77) (78) (79)) 
+    ((1/4)) 
+    ((60) (61) (62) (63) (64) (65) (66) (67) (68) (69) (70) (71) (72) (73) (74) (75) (76) (77) (78) (79)))))
 
 
 
-(cluster-engine-score
-(ce:ClusterEngine 
- 21 t nil 
- (append (ce:R-metric-hierarchy 0 :durations)          
-         (ce:r-note-meter #'(lambda (x) (progn (print x)
-                                       (if (equal '(4 4) (third x))
-                                         (if (member (mod (fourth x) 12)
-                                                     '(0 2 4 5 7 9 11)) t nil)
-                                         (if (member (mod (fourth x) 12)
-                                                     '(1 3 5 6 8 10 11)) t nil)  )))
-                       
-                       0 :d_offs_m_n :beats :incl-rests :normal :true/false)         
-         (ce:r-pitches-one-voice #'(lambda (x y)
-                                  (< (abs (- x y)) 5)                                  
-                                  )
-                              0 :pitches)         
-         )
- (ce:metric-domain '(4 4) '(1 2 3 4) nil '(6 8) '(1 3) 3/8)  
- '(((1/12)(1/16)(1/8)(1/4)) ((60) (61) (62) (63) (64) (65) (66) (67) (68) (69) (70) (71) (72) (73) (74) (75) (76) (77) (78) (79)))))
+(preview-cluster-engine-score
+ (ce:ClusterEngine 
+  21 t nil 
+  (append (ce:R-metric-hierarchy 0 :durations)          
+          (ce:r-note-meter #'(lambda (x) (progn (print x)
+                                           (if (equal '(4 4) (third x))
+                                             (if (member (mod (fourth x) 12)
+                                                         '(0 2 4 5 7 9 11)) t nil)
+                                             (if (member (mod (fourth x) 12)
+                                                         '(1 3 5 6 8 10 11)) t nil)  )))                           
+                           0 :d_offs_m_n :beats :incl-rests :normal :true/false)         
+          (ce:r-pitches-one-voice #'(lambda (x y)
+                                      (< (abs (- x y)) 5)                                  
+                                      )
+                                  0 :pitches)         
+          )
+  (ce:metric-domain '(4 4) '(1 2 3 4) nil '(6 8) '(1 3) 3/8)  
+  '(((1/12)(1/16)(1/8)(1/4)) ((60) (61) (62) (63) (64) (65) (66) (67) (68) (69) (70) (71) (72) (73) (74) (75) (76) (77) (78) (79)))))
 
 
 
