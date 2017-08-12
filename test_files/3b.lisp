c25a9de084bc279954ad34dc8dff400297bbf91d
Torsten Anders <torsten.anders@beds.ac.uk>
2017-08-12 21:39:05 +0200

    Added test file

---
+++ test_files/3b.lisp
@@ -0,0 +1,23 @@
+
+(in-package :om) 
+
+(preview-cluster-engine-score
+ (ce:clusterengine 
+  16 t nil 
+  '()  ; no rules
+  '((4 4)) 
+  '(((1/4))
+    ;; chord domain
+    (((67 71 74)) ((69 72 76)))
+    )))
+
+(preview-cluster-engine-score
+ (ce:clusterengine 
+  16 t nil 
+  '()  ; no rules
+  '((4 4)) 
+  '(((1/4) (1/8))
+    ;; chord sequence motif
+    (((67 71 74) (69 72 76)))
+    )))
+
