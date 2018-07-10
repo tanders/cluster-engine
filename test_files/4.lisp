a5a8fa7c32cf323971dce61a9311bd8410340977
Orjansan <sandred@ad.umanitoba.ca>
2018-07-10 08:48:49 -0500

    Tutorials now checked until 7e

---
+++ test_files/4.lisp
@@ -0,0 +1,11 @@
+(in-package cluster-engine) 
+
+(print 
+		(cluster-engine::R-pitches-one-voice #'(lambda (x y) (> y x)) 0 :pitches)
+	)
+	
+
+	(print 
+		(cluster-engine::R-canon '(0 1) :rhythm 1/2 7) 
+	)
+
