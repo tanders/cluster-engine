7c62c0399652addb4af47954235d3a857ee0aace
Torsten Anders <torstenanders@gmx.de>
2019-10-22 17:49:03 +0200

    Added testing utils: get-starts, get-keyword-voices, is-note?, is-rest?, is-chord?, get-start, get-duration, get-pitch

--- test_files/unit-tests/utils.lisp
+++ test_files/unit-tests/utils.lisp
@@ -33,6 +33,54 @@
   (mapcar #'tu:mat-trans (tu:plist->pairs (butlast cluster-engine-solution))))
 
 
+(defun get-starts (rhythms)
+  "Expects result of GET-RHYTHMS and return a list of start time lists, where each start time is a ratio."
+  (loop for durs in rhythms
+     collect (butlast ; skip end of last note
+	      (tu:dx->x durs 0))))
+
+
+(defun get-keyword-voices (cluster-engine-solution)
+  "Return list of voices, with each voice is represented as a list of notes/chords/rests in the form (:start <start-time> :duration <duration> :pitch <pitch>)."
+  (let* ((durationss (get-rhythms cluster-engine-solution))
+	 (startss (get-starts durationss))
+	 (pitchess (get-pitches cluster-engine-solution)))
+    ;; (mapcar #'tu:mat-trans startss durationss pitchess)
+    (loop
+       for durs in durationss
+       for starts in startss
+       for pitches in pitchess
+       collect (loop for (start dur pitch) in (tu:mat-trans (list starts durs pitches))
+		    collect (list :start start :duration dur :pitch pitch)))
+    ))
+
+
+(defun is-note? (event)
+  (numberp (get-pitch event)))
+
+
+(defun is-rest? (event)
+  (equal (get-pitch event) NIL))
+
+
+(defun is-chord? (event)
+  (let ((pitches (get-pitch event)))
+    (and (listp pitches)
+	 (numberp (first pitches)))))
+
+
+(defun get-start (event)
+  (getf event :start))
+
+
+(defun get-duration (event)
+  (getf event :duration))
+
+
+(defun get-pitch (event)
+  (getf event :pitch))
+
+
 (defun all-elements-equal? (xs &key (test #'equal))
   "Returns T if all elements in xs are equal."
   (let ((first-elt (first xs)))
