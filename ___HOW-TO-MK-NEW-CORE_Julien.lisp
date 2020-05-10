40dd1e001a4b2cabadacb192abf917f04637d009
JulienVincenot <julien.vincenot@gmail.com>
2020-05-09 18:21:44 -0700

    Useless with asdf in MOZ
    Useless with asdf in MOZ

--- ___HOW-TO-MK-NEW-CORE_Julien.lisp
+++
@@ -1,4 +0,0 @@
-sbcl --core /Users/julienvincenot/Documents/Dev-GitHub/cluster-engine/moz-complete.core 
-                ;;;;;; path to the last version of moz-complete core
-     --script /Users/julienvincenot/Documents/Dev-GitHub/cluster-engine/__package.lisp 
-                ;;;;;; path to the package file containing all paths of cluster-engine library
